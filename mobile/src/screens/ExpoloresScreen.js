b754c1cd67b2cf8354d3fd94e209d25639f36e44
Le Quoc Duong <lqd@LQDs-iMac.local>
2018-02-04 19:12:50 +0700

    done part 8

---
+++ mobile/src/screens/ExpoloresScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View`
+
+`;
+
+const T = styled.Text`
+
+`;
+
+class Explores extends Component {
+        render() {
+                return (
+                        <Root>
+                                <T>Explores</T>
+                        </Root>
+                );
+        }
+}
+
+export default Explores;
---
+++ mobile/src/screens/NotificationsScreen.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+
+const Root = styled.View`
+
+`;
+
+const T = styled.Text`
+
+`;
+
+class NotificationsScreen extends Component {
+        render() {
+                return (
+                        <Root>
+                                <T>NotificationsScreen</T>
+                        </Root>
+                );
+        }
+}
+
+export default NotificationsScreen;
