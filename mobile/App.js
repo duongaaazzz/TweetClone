52a92eac9a634dc418640a3670e904b76c68dd7d
Le Quoc Duong <lqduong@Les-iMac.local>
2018-03-12 17:40:06 +0700

    screen loading await check token

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { UIManager } from 'react-native';
+import { UIManager, AsyncStorage } from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { ThemeProvider } from 'styled-components';
+import { AppLoading } from 'expo';
 
 import { store, client } from './src/store';
 import { colors } from './src/utils/constants';
+import { login } from './src/actions/user';
 
 import AppNavigation from './src/navigations';
 
 if (UIManager.setLayoutAnimationEnabledExperimental) {
-        UIManager.setLayoutAnimationEnabledExperimental(true);
+    UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
 export default class App extends React.Component {
-        render() {
-                return (
-                        <ApolloProvider store={store} client={client}>
-                                <ThemeProvider theme={colors}>
-                                        <AppNavigation />
-                                </ThemeProvider>
-                        </ApolloProvider>
-                );
+
+    state = {
+        isAppReady: false
+    }
+
+    componentWillMount() {
+        this._isToken();
+    }
+
+    _isToken = async () => {
+        try {
+            const token = await AsyncStorage.getItem('attwittercloneapplication');
+
+            if (token != null) {
+                store.dispatch(login());
+            }
+        } catch (error) {
+            throw error;
         }
+
+        this.setState({ isAppReady: true });
+    }
+
+    render() {
+        if (!this.state.isAppReady) {
+            return <AppLoading />
+        }
+
+        return (
+            <ApolloProvider store={store} client={client}>
+                <ThemeProvider theme={colors}>
+                    <AppNavigation />
+                </ThemeProvider>
+            </ApolloProvider>
+        );
+    }
 }
